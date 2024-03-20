d4cb767d729c232d498c080e5f4414ab03d48049
hasanKryym <hasan.kryym@gmail.com>
2024-03-20 22:29:11 +0200

    styles fix

--- client/src/pages/Cart/Cart.jsx
+++ client/src/pages/Cart/Cart.jsx
@@ -21,36 +21,47 @@ const Cart = () => {
             <div className="head">
               <h2>
                 Shopping Cart{" "}
-                <span
-                  onClick={() => setShowOrderSummary(true)}
-                  className="view-summary"
-                >
-                  view summary
-                </span>
+                {cartProductsDetails.length !== 0 && (
+                  <span
+                    onClick={() => setShowOrderSummary(true)}
+                    className="view-summary"
+                  >
+                    view summary
+                  </span>
+                )}
               </h2>
             </div>
             <span>{cartProductsDetails.length} items</span>
           </div>
           <div className="underline-sky-blue"></div>
-          <div className="cart-details">
-            <table>
-              <th>Product Details</th>
-              <th>Quantity</th>
-              <th>Price</th>
-              <th>Total</th>
-              {cartProductsDetails.map((product) => {
-                return <CartProduct product={product} />;
-              })}
-            </table>
-          </div>
-        </div>
-        <div className="middle two-rem">
-          <button
-            onClick={() => setShowOrderSummary(true)}
-            className="custom-button"
-          >
-            CHECKOUT
-          </button>
+
+          {cartProductsDetails.length !== 0 ? (
+            <>
+              <div className="cart-details">
+                <table>
+                  <th>Product Details</th>
+                  <th>Quantity</th>
+                  <th>Price</th>
+                  <th>Total</th>
+                  {cartProductsDetails.map((product) => {
+                    return <CartProduct product={product} />;
+                  })}
+                </table>
+              </div>
+              <div className="middle two-rem">
+                <button
+                  onClick={() => setShowOrderSummary(true)}
+                  className="custom-button"
+                >
+                  CHECKOUT
+                </button>
+              </div>
+            </>
+          ) : (
+            <div className="middle two-rem">
+              <p>No Items!</p>
+            </div>
+          )}
         </div>
       </div>
       {showOrderSummary && <OrderSummary onClose={closeSummary} />}
