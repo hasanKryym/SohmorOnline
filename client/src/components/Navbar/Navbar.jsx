d849afb1e30b10677edf72bbe15f515b78a4b39d
hasanKryym <hasan.kryym@gmail.com>
2024-04-08 07:26:43 +0300

    responsive navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -5,10 +5,14 @@ import { FaUser } from "react-icons/fa";
 import { Link, useLocation } from "react-router-dom";
 import { pagesLinks } from "./pagesLinksEnum";
 import { useUser } from "../../context/User/UserContext";
+import { useState } from "react";
+import { FaBars } from "react-icons/fa";
+import { FaBarsStaggered } from "react-icons/fa6";
 
 const Navbar = () => {
   const { user } = useUser();
   const location = useLocation();
+  const [showSidebar, setShowSidebar] = useState(false);
 
   const navbarLinks = [
     {
@@ -49,7 +53,25 @@ const Navbar = () => {
     <>
       <nav className="navbar">
         <img className="Logo" src={Logo} alt="" />
-        <ul className="nav-list">
+        {showSidebar ? (
+          <FaBarsStaggered
+            className="nav_bars"
+            onClick={() => setShowSidebar(false)}
+          />
+        ) : (
+          <FaBars
+            className="nav_bars"
+            onClick={() => {
+              setShowSidebar(true);
+            }}
+          />
+        )}
+
+        <ul
+          className={`nav-list ${
+            showSidebar ? "slide-in-left" : "slide-out-left"
+          }`}
+        >
           {navbarLinks.map((navLink, i) => {
             return (
               <li className="list-item" key={i}>
